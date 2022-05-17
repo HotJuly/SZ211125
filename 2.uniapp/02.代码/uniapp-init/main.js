37c58fd7a074c511091836deb960fe543c8bb8e0
HotJuly <949588380@qq.com>
2022-05-17 16:26:38 +0800

    07.HBuilderX插件安装

--- 2.uniapp/02.代码/uniapp-init/main.js
+++ 2.uniapp/02.代码/uniapp-init/main.js
@@ -1,21 +1,12 @@
 import App from './App'
 
-// #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
+
+// 声明App组件为当前小程序
 App.mpType = 'app'
+
 const app = new Vue({
     ...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
--- 2.uniapp/02.代码/uniapp-init/pages/index/index.vue
+++ 2.uniapp/02.代码/uniapp-init/pages/index/index.vue
@@ -1,52 +1,75 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class= "indexContainer">
+		<view class="header">
+			<image class="logo" src="../../static/images/logo.png" mode=""></image>
+			<view class="search">
+				<i class="iconfont icon-sousuo"></i>
+				<input 
+				class="searchInput" 
+				type="text" 
+				value="" 
+				placeholder-class="placeholder"
+				placeholder="搜索商品"/>
+			</view>
+			<button class="username">七月</button>
 		</view>
 	</view>
+	<!-- <div>
+		<div>123</div>
+		<div>123</div>
+	</div> -->
+	<!-- uniapp同时兼容小程序和HTML的标签,推荐使用小程序的 -->
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title:'Hello43'
 			}
 		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
+		onLoad() {},
+		methods:{}
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="stylus">
+	// tab向后缩紧,shift+tab向前缩进
+	.indexContainer
+		.header
+			display flex
+			// margin-top 20upx
+			padding-top 20upx
+			.logo
+				width 118upx
+				height 40upx
+				margin 0 20upx
+				flex-shrink  0
+			.search
+				// width 100%
+				position relative
+				background-color  #ccc
+				height 60upx
+				flex-grow 1
+				border-radius  10upx
+				padding-left 60upx
+				.iconfont
+					position absolute
+					left 20upx
+					top 50%
+					transform translateY(-50%)
+				.searchInput
+					height 100%
+					// text-align center
+					.placeholder
+						text-align center
+						font-size 24upx
+						text-indent -40upx
+			.username
+				width 140upx
+				height 60upx
+				margin 0 20upx
+				font-size 24upx
+				flex-shrink  0
+				color red
 </style>
