65e8871fe447359bd88e9e77d26fc0526474a1c5
HotJuly <949588380@qq.com>
2022-06-01 09:26:54 +0800

    11.Promise复习(1)

--- 3.复习/02.代码/01.js基础/09.event loop.js
+++ 3.复习/02.代码/01.js基础/09.event loop.js
@@ -2,32 +2,72 @@
     定时器的延迟时间,最小值是1
         即便写的0,默认值也会变为1
 
-
+    node中一共具有6个宏任务队列,2个微任务队列(.then,nextTick各一个)
 */
 
-const fs = require('fs');
+// const fs = require('fs');
 
-setTimeout(()=>{
-    console.log('1');
-},0)
+// setTimeout(()=>{
+//     console.log('1');
+// },50)
 
-fs.readFile('./01.原型扩展(1).html',()=>{
-    console.log('2')
+// fs.readFile('./01.原型扩展(1).html',()=>{
+//     console.log('2')
 
-    setTimeout(()=>{
-        console.log('3');
-    },0)
+//     setTimeout(()=>{
+//         console.log('3');
+//     },0)
 
-    setImmediate(()=>{
-        console.log('4');
-    })
-})
+//     setImmediate(()=>{
+//         console.log('4');
+//     })
+// })
 
-setImmediate(()=>{
-    console.log('5');
-})
+// setImmediate(()=>{
+//     console.log('5');
+// })
 
 // 避免主线程代码执行速度过快,小于1ms,导致定时器不能成功触发
-for (let index = 0; index < 100000; index++) {
+// for (let index = 0; index < 100000; index++) {
     
-}
+// }
+
+// .then开启的微任务已经是VIP了,而nextTick是SVIP
+
+// Promise.resolve().then(()=>{
+//     console.log(1)
+
+
+//     Promise.resolve().then(()=>{
+//         console.log(2)
+//     })
+
+//     process.nextTick(()=>{
+//         console.log(3)
+//     })
+
+//     Promise.resolve().then(()=>{
+//         console.log(4)
+//     })
+// })
+
+// Promise.resolve().then(()=>{
+//     console.log(5)
+// })
+
+process.nextTick(()=>{
+    console.log(1)
+
+
+    Promise.resolve().then(()=>{
+        console.log(2)
+    })
+
+    process.nextTick(()=>{
+        console.log(3)
+    })
+
+    Promise.resolve().then(()=>{
+        console.log(4)
+    })
+})
